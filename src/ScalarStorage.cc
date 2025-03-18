34eba8b50c225358259e5cbc5577e49e1e0f5406
luwhy <1023412173@qq.com>
2025-03-18 18:16:01 +0800

    commit

--- src/ScalarStorage.cc
+++ src/ScalarStorage.cc
@@ -1,1 +1,17 @@
 #include "ScalarStorage.h"
+
+ScalarStorage::ScalarStorage(const std::string &db_path)
+{
+}
+ScalarStorage::~ScalarStorage()
+{
+}
+
+void ScalarStorage::insert_scalar(uint64_t id, const rapidjson::Document &data)
+{
+}
+
+rapidjson::Document ScalarStorage::get_scalar(uint64_t id)
+{
+    return rapidjson::Document();
+}
--- src/ScalarStorage.h
+++ src/ScalarStorage.h
@@ -3,8 +3,32 @@
 #include <string>
 #include <vector>
 #include <rapidjson/document.h>
+#include <rocksdb/db.h>
 class ScalarStorage
 {
+    /**
+     * @brief 构造函数，打开rocksdb
+     *
+     * @param db_path
+     */
     ScalarStorage(const std::string &db_path);
+
+    /**
+     * @brief Destroy the Scalar Storage object
+     *
+     */
+    ~ScalarStorage();
+    /**
+     * @brief 向量插入函数
+     *
+     * @param id
+     * @param data
+     */
+    void insert_scalar(uint64_t id, const rapidjson::Document &data);
+
+    rapidjson::Document get_scalar(uint64_t id);
+
+private:
+    rocksdb::DB *db;
 };
 #endif
